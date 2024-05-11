bd09ae3b0d37b61cd5b60ca7c7636f50d44bef79
mitian233 <mitian233@yahoo.co.jp>
2024-05-11 22:08:55 +0800

    characters' page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-
 import netlify from "@astrojs/netlify";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()],
+  integrations: [tailwind(), vue(), db()]
   // output: "server",
   // adapter: netlify()
 });
